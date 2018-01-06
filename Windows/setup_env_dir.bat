e24a5537dd1ebe339c40e58d97ca82f22f8d6b84
Adrian Muntean <m.adr.home@gmail.com>
2018-01-06 14:49:39 -0800

    Add setupt for Scripts dir

--- Windows/setup_env_dir.bat
+++ Windows/setup_env_dir.bat
@@ -2,7 +2,9 @@
  :: Create working dirs
  if not exist "%USERPROFILE%\Source\" mkdir %USERPROFILE%\Source\
  if not exist "%USERPROFILE%\Tools\" mkdir %USERPROFILE%\Tools\
+ if not exist "%USERPROFILE%\Scripts\" mkdir %USERPROFILE%\Scripts\
  
  :: Update user variables
  setx SOURCE "%USERPROFILE%\Source"
  setx TOOLS "%USERPROFILE%\Tools"
+ setx SCRIPTS "%USERPROFILE%\Scripts"
